174c1d3b58c328f3318d1695f8156d59167b0a58
Osborn Huang <osbornjmhuang@gmail.com>
2018-08-31 17:34:12 +0800

    project init

---
+++ server.js
@@ -0,0 +1,12 @@
+'use strict';
+const express = require('express');
+const path = require('path');
+const serveStatic = require('serve-static');
+
+let app = express();
+app.use(serveStatic(__dirname + "/dist"));
+
+const port = process.env.PORT || 80;
+app.listen(port, () => {
+    console.log('Listening on port ' + port)
+});
